83fd590c6ee0948a120fa45b0dad696343fbee54
1894475 <1894475@E21034.CEGEPGIM.LCL>
2019-02-09 10:44:06 -0500

    Sruthi - created specialorder class with methods (also with extends)

---
+++ Team6/src/team6/SpecialOrder.java
@@ -0,0 +1,24 @@
+ /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team6;
+
+import java.util.Date;
+
+/**
+ *
+ * @author 1894475
+ */
+public class SpecialOrder extends Order
+{
+
+    public SpecialOrder(Date date, String number) {
+        super(date, number);
+    }
+   public void dispatch()
+   {
+       
+   }
+}
