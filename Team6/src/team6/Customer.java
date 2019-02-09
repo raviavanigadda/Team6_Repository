37a260960c7a5fef68d9c5831f577072478df607
1895198 <1895198@E21056.CEGEPGIM.LCL>
2019-02-09 10:28:34 -0500

    Shriya - Created Customer class methods

---
+++ Team6/src/team6/Customer.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team6;
+
+/**
+ *
+ * @author 1895198
+ */
+public class Customer 
+{
+    String name,location;
+    
+public Customer(String name, String location)
+{
+ 
+}
+public void setName(String name)
+{
+    
+}
+public void getName()
+{
+    
+}
+public void setLocation(String location)
+{
+    
+}
+public void getLocation()
+{
+    
+}
+public void display()
+{
+    
+}
+}
