67305fb38166f564a4cc4aef7baa0b8cad6761ef
1895126 <1895126@E21031.CEGEPGIM.LCL>
2019-02-09 10:29:40 -0500

    declare class

---
+++ Team6/src/team6/NormalOrder.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team6;
+
+import java.util.*;
+
+/**
+ *
+ * @author 1895126
+ */
+public class NormalOrder {
+    
+    public Date date;
+    public String number;
+    
+    public NormalOrder(Date date,String number){
+        
+        this.date=date;
+        this.number=number;
+    }
+    
+    public void confirm(){
+        
+    }
+    
+    public void close(){
+        
+    }
+    
+    public void dispatch(){
+        
+    }
+    public void receive(){
+        
+    }
+}
